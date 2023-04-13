b808c86e8f4e0a9dc9a44a559261aa8668619e96
Todd David Rudick <rudick@gmail.com>
2023-04-13 16:09:55 -0600

    quick fix

--- includeHTML.js
+++ includeHTML.js
@@ -33,7 +33,7 @@ function includeBody() {
   if (filename == "index" || filename == "") {
     filename = "about";
   }
-  if (filename == "about" || filename == "trading" || filename == "backtest" || filename == "optimization") {
+  if (filename == "about" || filename == "trading" || filename == "backtesting" || filename == "optimization") {
     window.document.write("<div w3-include-html=\"");
     window.document.write(filename);
     window.document.write("-body.html\"></div>");
